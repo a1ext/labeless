4c75e29b438db233c333d0733c50bb9315547c10
a1ext <a13x4nd3r.t@gmail.com>
2016-03-22 18:31:33 +0300

    Version bumped up

--- common/version.h
+++ common/version.h
@@ -8,22 +8,27 @@
 
 #pragma once
 
-
 #define _MAKE_VER(A, B, C, D) ((unsigned((A) & 0xFF) << 24) | (unsigned((B) & 0xFF) << 16) | (unsigned((C) & 0xFF) << 8) | ((D) & 0xFF))
-#define _STR(X) #X
-#define STR(X) _STR(X)
+#ifdef _UNICODE
+#	define __TTT(X) L ## X
+#	define _SSTR(X) __TTT(#X)
+#else // _UNICODE
+#	define __TTT(X) X
+#	define _SSTR(X) #X
+#endif // _UNICODE
 
+#define SSTR(X) _SSTR(X)
 
 
 #define VERSION_MAJOR               1
-#define VERSION_MINOR               0
+#define VERSION_MINOR               1
 #define VERSION_REVISION            0
-#define VERSION_BUILD               8
+#define VERSION_BUILD               1
 
 
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
 
 #define LABELESS_VER_INT            _MAKE_VER(VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD)
-#define LABELESS_VER_STR            STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_REVISION) "." STR(VERSION_BUILD)
+#define LABELESS_VER_STR            SSTR(VERSION_MAJOR) __TTT(".") SSTR(VERSION_MINOR) __TTT(".") SSTR(VERSION_REVISION) __TTT(".") SSTR(VERSION_BUILD)
 
 #define LABELESS_PRODUCT            "Labeless"
