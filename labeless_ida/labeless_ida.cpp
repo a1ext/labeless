0120ffd07e340fef1fea12c76297c80b664fe2b5
a1ext <a13x4nd3r.t@gmail.com>
2018-09-22 02:23:29 +0300

    [IDA] Enabled support of a `Binary` file type for `Intel 80x86` platform that was asked by @sisoma2 (issue #38) and @marklech

--- labeless_ida/labeless_ida.cpp
+++ labeless_ida/labeless_ida.cpp
@@ -2905,7 +2905,7 @@ hook_cb_t_ret_type_t Labeless::idp_callback(void* /*user_data*/, int notificatio
 	case PROCESSOR_T_NEWFILE:
 	case PROCESSOR_T_OLDFILE:
 		do {
-			const bool compat = ph.id == PLFM_386 && inf.filetype == f_PE;
+			const bool compat = ph.id == PLFM_386 && (inf.filetype == f_PE || inf.filetype == f_BIN);
 			if (compat)
 			{
 				ll.setEnabled();
